a31d3fdeb8067fa4f9a5e1166f4608dec0baa7db
Andrew Rempe <andrewrempe@gmail.com>
2015-01-22 11:47:45 -0500

    Adds welcome screen

--- craps.cpp
+++ craps.cpp
@@ -5,20 +5,37 @@
 	Plays the game craps with the user
 */
 
-// includes
+// -- includes
 #include "dice.h"
 #include <iostream>
 using namespace std;
 
-// prototypes
+// -- prototypes
 
-// constants
+// prints out a welcome message with information on how to play
+void welcome();
+
+// -- constants
 enum GameState { WAGER, ROLLING, WIN, LOSE };
 
 int main() {
 
-	Dice d;
+	welcome();
+}
 
-	d.roll();
-	d.print();
+void welcome() {
+
+	cout << endl;
+	cout << "**************************************************" << endl;
+	cout << "* ---------------- HOW TO PLAY ----------------- *" << endl;
+	cout << "* A player rolls two dice. If the sum is 7 or 11 *" << endl;
+	cout << "* on the first roll, the player wins. If the sum *" << endl;
+	cout << "* is 2,3, or 12 (called craps), the player loses *" << endl;
+	cout << "* If the sum is 4, 5, 6, 8, 9 or 10, then that   *" << endl;
+	cout << "* sum becomes the player's 'point.' To win you   *" << endl;
+	cout << "* must continue to roll until you get your point *" << endl;
+	cout << "* If you roll a 7 before you get your point, you *" << endl;
+	cout << "* lose the round.                                *" << endl;
+	cout << "**************************************************" << endl;
+	cout << endl;
 }
